e288ec2483a5248a5fcf0a9b3a20e3755539c3e1
WalterB-PB <walter.benitez286@pascualbravo.edu.co>
2026-09-09 20:23:34 -0500

    Update app_nivel_cornare.py

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -124,7 +124,7 @@ USOS_AGUA = [
         "banda_afectacion": "P10",
         "poblacion_estimada": 60,
         "hectareas": 25,
-        "fuente": "Estimacion",
+        "fuente": "Estimado",
     },
     {
         "nombre": "Acueductos veredales (captaciones menores)",
